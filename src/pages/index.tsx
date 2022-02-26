cf61eea3ebbcd40937a90ba85d9d8c2ccd319e2a
Zerthox <zerthox@gmail.com>
2022-02-26 17:40:38 +0100

    Split boxes into memo data & checked state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useMemo} from "react";
 import {Layout, Grid, spacing} from "../components/layout";
 import {Link, Paragraph, Checkbox, LinkButton} from "../components/elements";
 import {encode} from "../convert/v1";
@@ -7,15 +7,15 @@ import {useFractals, useTodaysDailies, useTodaysRecs, scaleToTier, useFields, ra
 interface Box {
     display: string;
     fractal: string;
+    hasCM: boolean;
     isCM: boolean;
     isDaily: boolean;
     rec: number;
-    checked: boolean;
 }
 
-const toFields = (fields: Field[], boxes: Box[]): Field[] => {
-    const hasDailies = boxes.some(({isCM, checked}) => checked && !isCM);
-    const hasCMs = boxes.some(({isCM, checked}) => checked && isCM);
+const toFields = (fields: Field[], boxes: Box[], checked: boolean[]): Field[] => {
+    const hasDailies = boxes.some(({isCM}, i) => checked[i] && !isCM);
+    const hasCMs = boxes.some(({isCM}, i) => checked[i] && isCM);
 
     return fields.filter(({fractal, mode}) => {
         switch (fractal) {
@@ -24,8 +24,8 @@ const toFields = (fields: Field[], boxes: Box[]): Field[] => {
             case "Dailies":
                 return hasDailies;
             default:
-                return boxes.some((box) => (
-                    box.checked
+                return boxes.some((box, i) => (
+                    checked[i]
                     && box.fractal === fractal
                     && (
                         mode === Mode.Both
@@ -37,32 +37,42 @@ const toFields = (fields: Field[], boxes: Box[]): Field[] => {
     });
 };
 
-const App = (): JSX.Element => {
+const Index = (): JSX.Element => {
+    // data
     const fractals = useFractals();
     const dailies = useTodaysDailies();
     const recs = useTodaysRecs();
     const fields = useFields();
 
-    const [boxes, setBoxes] = useState(() => [
-        ...fractals.map(({id, name, display, scales, hasCM}) => ({
-            fractal: name,
-            display: display ?? name,
-            isCM: false,
-            isDaily: dailies.includes(id),
-            rec: scaleToTier(scales.find((scale) => recs.includes(scale)) ?? 0),
-            checked: !hasCM && dailies.includes(id)
-        })).sort((a, b) => a.display.localeCompare(b.display)),
-        ...fractals.filter(({hasCM}) => hasCM).map(({id, name, displayCM}) => ({
-            fractal: name,
-            display: `${displayCM ?? name} CM`,
-            isCM: true,
-            isDaily: dailies.includes(id),
-            rec: 0,
-            checked: true
-        }))
-    ]);
+    // checkboxes
+    const boxes = useMemo<Box[]>(() => [
+        ...fractals
+            .map(({id, name, display, scales, hasCM}) => ({
+                fractal: name,
+                display: display ?? name,
+                isCM: false,
+                hasCM,
+                isDaily: dailies.includes(id),
+                rec: scaleToTier(scales.find((scale) => recs.includes(scale)) ?? 0)
+            }))
+            .sort((a, b) => a.display.localeCompare(b.display)),
+        ...fractals
+            .filter(({hasCM}) => hasCM)
+            .map(({id, name, displayCM}) => ({
+                fractal: name,
+                display: `${displayCM ?? name} CM`,
+                hasCM: true,
+                isCM: true,
+                isDaily: dailies.includes(id),
+                rec: 0
+            }))
+    ], [fractals, dailies, recs]);
 
-    const genRand = () => encode(randomFields(fields, toFields(fields, boxes)));
+    // checkbox state
+    const [checked, setChecked] = useState(() => boxes.map(({hasCM, isCM, isDaily}) => isCM || (!hasCM && isDaily)));
+
+    // random field ids
+    const genRand = () => encode(randomFields(fields, toFields(fields, boxes, checked)));
     const [rand, setRand] = useState(genRand);
 
     return (
@@ -71,17 +81,17 @@ const App = (): JSX.Element => {
             <LinkButton
                 to={`/v1/card?${rand}`}
                 className={spacing.bottom20}
-                onMouseDown={() => setRand(genRand)}
+                onMouseDown={() => setRand(genRand())}
             >Generate Bingo</LinkButton>
             <Grid className={spacing.bottom20}>
-                {boxes.map(({display, isDaily, rec, checked}, i) => (
+                {boxes.map(({display, isDaily, rec}, i) => (
                     <Checkbox
                         key={i}
-                        checked={checked}
-                        onChange={(checked) => {
-                            boxes[i].checked = checked;
-                            setBoxes([...boxes]);
-                            setRand(genRand);
+                        checked={checked[i]}
+                        onChange={(value) => {
+                            checked[i] = value;
+                            setChecked([...checked]);
+                            setRand(genRand());
                         }}
                     >
                         {display}
@@ -104,4 +114,4 @@ const App = (): JSX.Element => {
     );
 };
 
-export default App;
+export default Index;
